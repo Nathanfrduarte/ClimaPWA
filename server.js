210dc2ea3dc6bfda60b713d4b3a74a9388c8f75e
nathanfrduarte <nathan.duarte51.nd@gmail.com>
2019-10-06 09:13:25 -0300

    v1.3.0

--- server.js
+++ server.js
@@ -7,27 +7,19 @@ const FORECAST_DELAY = 0;
 const API_KEY = 'd9c10be08af503c0c64748b3c4a95076';
 const BASE_URL = `https://api.darksky.net/forecast`;
 
-/**
- * Generates a fake forecast in case the weather API is not available.
- *
- * @param {String} location GPS location to use.
- * @return {Object} forecast object.
- *
- * Gets the weather forecast from the Dark Sky API for the given location.
- *
- * @param {Request} req request object from Express.
- * @param {Response} resp response object from Express.
- */
+// req: objeto de requisição do Express
+// resp: objeto de resposta do Express
 
-
-function getForecast(req, resp) {
+function getPrevisao(req, resp) {
+  // Variáveis utilizadas na requisição
   const location = req.params.location || "-19.9023386,-44.1041379";
   const exclude = "?exclude="; //?exclude=minutely,hourly,daily,alerts,flags
   const language = "?lang=pt";
   const unit = "?units=auto";
-  
+  // URL da requisição
   const url = `${BASE_URL}/${API_KEY}/${location}/${exclude}${language}${unit}`
 
+  // Buscar requisição e tratamento de erro, caso houver, da resposta
   fetch(url).then((resp) => {    
     if (resp.status !== 200) {
       throw new Error(resp.statusText);
@@ -38,38 +30,23 @@ function getForecast(req, resp) {
       resp.json(data);
     }, FORECAST_DELAY);
   }).catch((err) => {
-    console.error('Dark Sky API Error:', err.message);
+    console.error('Erro API Dark Sky:', err.message);
   });
 }
 
-/**
- * Starts the Express server.
- *
- * @return {ExpressServer} instance of the Express server.
- */
+// Instância do Express Server
 function startServer() {
   const app = express();
 
-  // Redirect HTTP to HTTPS,
+  // Redirecionar HTTP para HTTPS,
   app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
 
-  // Logging for each request
-  app.use((req, resp, next) => {
-    const now = new Date();
-    const time = `${now.toLocaleDateString()} - ${now.toLocaleTimeString()}`;
-    const path = `"${req.method} ${req.path}"`;
-    const m = `${req.ip} - ${time} - ${path}`;
-    // eslint-disable-next-line no-console
-    console.log(m);
-    next();
-  });
+  // Requisições para os dados
+  app.get('/forecast/:location', getPrevisao);
+  app.get('/forecast/', getPrevisao);
+  app.get('/forecast', getPrevisao);
 
-  // Handle requests for the data
-  app.get('/forecast/:location', getForecast);
-  app.get('/forecast/', getForecast);
-  app.get('/forecast', getForecast);
-
-  // Handle requests for static files
+  // Requisições para os arquivos estáticos
   app.use(express.static('public'));
   
   // Porta criada pelo Heroku ou porta local
@@ -77,6 +54,7 @@ function startServer() {
   // Start the server
   return app.listen(porta);
     
+  // Heroku não aceita a requisição abaixo, comentada somente para uso local
   // return app.listen('8000', () => {
   //   // eslint-disable-next-line no-console
   //   console.log('Servidor local iniciado na porta ' + porta + '...');
